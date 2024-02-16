edd84c1ef1a43103613dfaba3484c09042ff4707
Abhishekkumarsingh48 <143267528+Abhishekkumarsingh48@users.noreply.github.com>
2024-02-16 11:41:45 +0530

    Add files via upload

---
+++ Quotient and reminder.py
@@ -0,0 +1,6 @@
+'''write a program to read two numbers and print their Quotient and reminder'''
+a=int(input("enter 1st num"))
+b=int(input("enter 2nd num"))
+c=a//b
+d=a%b
+print("quotient of {} and {} is {}   AND reminder is {}".format(a,b,c,d))
